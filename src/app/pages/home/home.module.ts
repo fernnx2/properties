42f59b41c5ee4da3816a05a3a1db9c7e1d9184ad
fernnx2 <rene.menjivar@itproject41.com>
2021-03-03 09:28:57 -0600

    init config

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 @NgModule({
-  imports: [CommonModule,HomeRoutingModule],
+  imports: [CommonModule,HomeRoutingModule, NgbModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers:[]
